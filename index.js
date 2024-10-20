775b64c95f711f49c7bad0424baabd51f94644f4
HAnnahc77 <HAnnahc77@nycstudents.net>
2024-10-20 17:06:40 -0400

    commit

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const DOMSelectors = {
-  button: document.getElementById("button"),
   spacing: document.querySelector(".spacing"),
-  removebutton: document.querySelector(".removebutton"),
-  container: document.querySelector(".container"),
+  button: document.getElementById("button"),
   inputname: document.getElementById("inputname"),
   inputimg: document.getElementById("inputimg"),
   inputdesc: document.getElementById("inputdesc"),
@@ -10,32 +8,30 @@ const DOMSelectors = {
 
 DOMSelectors.button.addEventListener("click", function (event) {
   event.preventDefault();
-
   let input1 = DOMSelectors.inputname.value;
   let input2 = DOMSelectors.inputimg.value;
   let input3 = DOMSelectors.inputdesc.value;
 
-    const cardHTML = 
-    ` <div class="card">
+
+    if (input1 && input2 && input3) {
+      DOMSelectors.spacing.insertAdjacentHTML("afterbegin",` <div class="card">
         <h1 id="card-header">${input1}</h1>
         <img src="${input2}" alt="Album Image">
         <h2>${input3}</h2>
         <button class="removebutton">Remove</button>
-      </div>`;
+      </div>`);
 
-    if (input1 && input2 && input3) {
-      DOMSelectors.spacing.insertAdjacentHTML("afterbegin", cardHTML);
-      DOMSelectors.container.classList.remove('hidden');
+    clearInputs();
     }
-
-    DOMSelectors.inputname.value = '';
-    DOMSelectors.inputimg.value = '';
-    DOMSelectors.inputdesc.value = '';
 });
+
+function clearInputs() {
+  DOMSelectors.inputname.value = '';
+  DOMSelectors.inputimg.value = '';
+  DOMSelectors.inputdesc.value = '';
+}
 
 DOMSelectors.spacing.addEventListener("click", function (event) {
   if (event.target.classList.contains("removebutton")) {
-    const cardToRemove = event.target.parentElement; 
-    cardToRemove.remove();
-  }
-});
+  event.target.parentElement.remove(); 
+  }});
